5a5a00c7941829f21292d6208cd1e3924ad969e2
Li Productions <172457372+goldstac@users.noreply.github.com>
2025-11-06 20:55:59 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-//coming soon
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+string username;
+cout << "Enter Username\n";
+getline(cin,username);
+if (username == "admin") {
+    cout << "Welcome Back Admin\n";
+}
+else{
+    cout << "Wrong Username\n";
+}
+return 0;
